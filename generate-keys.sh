b0344fbecf7d169bf2cba057949e729f86406ea1
Tay Ray Chuan <rtay@pivotal.io>
2018-06-25 15:15:26 +0800

    Generate keys for windows worker
    Signed-off-by: Natalie Tay <ntay@pivotal.io>
    Signed-off-by: Tay Ray Chuan <rtay@pivotal.io>

--- generate-keys.sh
+++ generate-keys.sh
@@ -8,6 +8,8 @@ yes | ssh-keygen -t rsa -f ./keys/web/tsa_host_key -N ''
 yes | ssh-keygen -t rsa -f ./keys/web/session_signing_key -N ''
 
 yes | ssh-keygen -t rsa -f ./keys/worker/worker_key -N ''
+yes | ssh-keygen -t rsa -f ./windows-worker/worker_key -N ''
 
-cp ./keys/worker/worker_key.pub ./keys/web/authorized_worker_keys
+cat ./keys/worker/worker_key.pub ./windows-worker/worker_key.pub > ./keys/web/authorized_worker_keys
 cp ./keys/web/tsa_host_key.pub ./keys/worker
+cp ./keys/web/tsa_host_key.pub ./windows-worker
